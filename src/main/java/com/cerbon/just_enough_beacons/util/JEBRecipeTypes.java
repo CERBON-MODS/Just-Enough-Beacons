847d98413a63cf44fc9d0b8d02fe5faffb657728
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-09-10 22:38:39 -0300

    change recipe category path name for beacon base block

--- src/main/java/com/cerbon/just_enough_beacons/util/JEBRecipeTypes.java
+++ src/main/java/com/cerbon/just_enough_beacons/util/JEBRecipeTypes.java
@@ -4,5 +4,5 @@ import com.cerbon.just_enough_beacons.jei.beaconbaseblock.BeaconBaseBlockRecipe;
 import mezz.jei.api.recipe.RecipeType;
 
 public class JEBRecipeTypes {
-    public static final RecipeType<BeaconBaseBlockRecipe> BEACON_BASE_BLOCK = RecipeType.create(JEBConstants.MOD_ID, "beacon_block_recipe", BeaconBaseBlockRecipe.class);
+    public static final RecipeType<BeaconBaseBlockRecipe> BEACON_BASE_BLOCK = RecipeType.create(JEBConstants.MOD_ID, "beacon_base_block", BeaconBaseBlockRecipe.class);
 }

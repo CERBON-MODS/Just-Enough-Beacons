cb6f2b8c1510692258a10fcdf43123258dd1659d
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-10-25 07:56:14 -0300

    Add compatibility with Double Slabs mod

--- src/main/java/com/cerbon/just_enough_beacons/jei/conduitbaseblock/ConduitFrameBlockRecipe.java
+++ src/main/java/com/cerbon/just_enough_beacons/jei/conduitbaseblock/ConduitFrameBlockRecipe.java
@@ -1,5 +1,8 @@
 package com.cerbon.just_enough_beacons.jei.conduitbaseblock;
 
+import cjminecraft.doubleslabs.common.blocks.DynamicSlabBlock;
+import com.cerbon.just_enough_beacons.util.JEBConstants;
+import com.cerbon.just_enough_beacons.util.JEBUtils;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -20,6 +23,11 @@ public record ConduitFrameBlockRecipe(int index) {
     }
 
     private static List<ItemStack> getConduitFrameBlocks() {
+        if (JEBUtils.isModLoaded(JEBConstants.DOUBLE_SLABS))
+            return ForgeRegistries.BLOCKS.getValues().stream()
+                    .filter(block -> !(block instanceof DynamicSlabBlock) && block.isConduitFrame(block.defaultBlockState(), null, null, null))
+                    .map(ItemStack::new).collect(Collectors.toList());
+
         return ForgeRegistries.BLOCKS.getValues().stream()
                 .filter(block -> block.isConduitFrame(block.defaultBlockState(), null, null, null))
                 .map(ItemStack::new).collect(Collectors.toList());
--- src/main/java/com/cerbon/just_enough_beacons/util/JEBConstants.java
+++ src/main/java/com/cerbon/just_enough_beacons/util/JEBConstants.java
@@ -8,5 +8,6 @@ public class JEBConstants {
     public static final ResourceLocation BACKGROUND = new ResourceLocation(MOD_ID, "textures/gui/arrow.png");
 
     public static final String BETTER_BEACONS = "better_beacons";
+    public static final String DOUBLE_SLABS = "doubleslabs";
 
 }

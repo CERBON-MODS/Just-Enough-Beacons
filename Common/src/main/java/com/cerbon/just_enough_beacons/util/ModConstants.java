1eeedd8c4037d2a6b57164c28014536352cb7e22
Felipe <felipecerboncinix@gmail.com>
2024-01-28 12:44:44 -0300

    Add space between lines

--- Common/src/main/java/com/cerbon/just_enough_beacons/util/ModConstants.java
+++ Common/src/main/java/com/cerbon/just_enough_beacons/util/ModConstants.java
@@ -6,5 +6,6 @@ import org.slf4j.Logger;
 public class ModConstants {
     public static final String MOD_ID = "just_enough_beacons";
     public static final String MOD_NAME = "Just Enough Beacons";
+
     public static final Logger LOGGER = LogUtils.getLogger();
 }
